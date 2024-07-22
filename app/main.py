a463b1a5b33a88af6c4ddd370c00eb10051180a6
unknown <merkoncel@gmail.com>
2024-07-22 17:46:06 +0300

    init

---
+++ app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+from contextlib import asynccontextmanager
+
+from database.database import create_tables, delete_tables
+from routers.router import router as tasks_router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await delete_tables()
+    print("База очищена")
+    await create_tables()
+    print("База готова к работе")
+    yield
+    print("Выключение")
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(tasks_router)
